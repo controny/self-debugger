0e3ce6534d18025d71778fc3a53d8641cf313435
controny <0755dimitri@gmail.com>
2024-03-28 13:16:58 -0500

    perf: use multiprocessing

--- run.py
+++ run.py
@@ -1,9 +1,34 @@
 import json
 import os
 from tqdm import tqdm
+import multiprocessing
+import traceback
 import argparse
 from self_debugger import SelfDebugger
 from dataset_loader import MBPPDatasetLoader
+
+
+def process_problem(inp):
+    i, problem, args = inp
+    description = problem['description']
+    test_list = problem['test_list']
+    
+    self_debugger = SelfDebugger(args.model_name,
+                                max_debugging_steps=args.max_debugging_steps,
+                                temperature=args.temperature)
+
+    try:
+        result = self_debugger.debug_code(description, test_list)
+        result.update({
+            'index': i,
+            'description': description,
+            'test_list': test_list,
+        })
+        return result
+    except Exception as e:
+        traceback.print_exc()
+        return None
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -19,14 +44,9 @@ if __name__ == "__main__":
         problems = problems[:5]
     print(f"Number of problems: {len(problems)}")
 
-    # Initialize the SelfDebugger
-    self_debugger = SelfDebugger(args.model_name,
-                                max_debugging_steps=args.max_debugging_steps,
-                                temperature=args.temperature)
-
-    # TODO use multiprocessing to speed up the process
     # Iterate through the problems, debug the code, and check if it passes the test cases
     num_success = 0
+    num_total = len(problems)
 
     save_path = 'outputs/results.jsonl'
     processed_indices = set()
@@ -37,23 +57,20 @@ if __name__ == "__main__":
                 processed_indices.add(result['index'])
                 # also update the success rate
                 num_success += int(result['success'])
+    
+    # Filter out the processed problems
+    problems = [problem for i, problem in enumerate(problems) if i not in processed_indices]
 
-    with open(save_path, 'a') as jsonfile:
-        for i, problem in tqdm(enumerate(problems)):
-            if i in processed_indices:
-                continue
+    num_workers = multiprocessing.cpu_count()
+    print(f"Number of workers: {num_workers}")
+    with multiprocessing.Pool(num_workers) as pool:
+        with tqdm(total=len(problems)) as pbar:
+            for result in pool.imap_unordered(process_problem, [(i, problem, args) for i, problem in enumerate(problems)]):
+                pbar.update()
+                if result is not None:
+                    with open(save_path, 'a') as jsonfile:
+                        num_success += int(result['success'])
+                        jsonfile.write(json.dumps(result) + '\n')
+                        jsonfile.flush()
 
-            description = problem['description']
-            test_list = problem['test_list']
-            result = self_debugger.debug_code(description, test_list)
-            num_success += int(result['success'])
-
-            result.update({
-                'index': i,
-                'description': description,
-                'test_list': test_list,
-            })
-            jsonfile.write(json.dumps(result) + '\n')
-            jsonfile.flush()
-
-    print(f"Success Rate: {num_success}/{len(problems)}")
+    print(f"Success Rate: {num_success}/{num_total} = {num_success / num_total:.2f}")
